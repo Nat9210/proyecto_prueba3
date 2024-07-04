ca81bdddfec56bd93f578fcb9953df4197248e0b
Nat9210 <nata.benitez.pra@gmail.com>
2024-07-04 13:14:54 -0400

    template agregado a myapp aun no conectado

--- myapp/views.py
+++ myapp/views.py
@@ -3,12 +3,29 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def hola(request):
-    return HttpResponse("holis")
-
-def categoria(request):
-    return HttpResponse("Categorias")
+def index(request):
+    return HttpResponse("index")
 
 def login(request):
-    return HttpResponse("registro")
+    return HttpResponse("login")
 
+def main(request):
+    return HttpResponse("main")
+
+def signup(request):
+    return HttpResponse("signup")
+
+def categories(request):
+    return HttpResponse("categories")
+
+def animewatch(request):
+    return HttpResponse("anime-watch")
+
+def animedetails(request):
+    return HttpResponse("anime-details")
+
+def blog(request):
+    return HttpResponse("blog")
+
+def blogdetails(request):
+    return HttpResponse("blog-details")
